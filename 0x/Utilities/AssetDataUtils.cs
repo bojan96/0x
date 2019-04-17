d70453b12497b89f9b529824285de6207f776e7b
Bojan Malinic <bojan.malinic@outlook.com>
2019-04-17 13:02:05 +0200

    Add project files.

---
+++ 0x/Utilities/AssetDataUtils.cs
@@ -0,0 +1,31 @@
+﻿using Nethereum.Util;
+using System;
+using System.Text;
+
+namespace ZeroX.Utilities
+{
+    public static class AssetDataUtils
+    {
+
+        private const string ERC20TokenSignature = "ERC20Token(address)";
+        private static readonly byte[] _erc20AssetHeader = Sha3Keccack.Current
+            .CalculateHash(Encoding.UTF8.GetBytes(ERC20TokenSignature)).Slice(0, 4);
+
+
+        /// <summary>
+        /// Encodes ERC20 contract address as per 0x specification
+        /// </summary>
+        /// <param name="address">Contract address</param>
+        /// <returns>Asset data</returns>
+        public static byte[] GetERC20AssetData(EthereumAddress address)
+        {
+            if (address == null)
+                throw new ArgumentNullException(nameof(address));
+
+            // Pad byte array to 32 bytes
+            byte[] arr = ByteUtil.Merge(new byte[12], address.Bytes);
+            return ByteUtil.Merge(_erc20AssetHeader, arr);
+        }
+
+    }
+}
---
+++ Tests/Tests/AssetDataUtilsTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nethereum.Hex.HexConvertors.Extensions;
+using ZeroX.Utilities;
+
+namespace Tests.Tests
+{
+    [TestClass]
+    public class AssetDataUtilsTest
+    {
+        [TestMethod]
+        public void TestERC20AssetDataEncoding()
+        {
+            byte[] assetData = AssetDataUtils.GetERC20AssetData(EthereumAddress.ZeroAddress);
+
+            byte[] expectedAssetData = ("0xf47261b00000000000000000000000000000000000000000000" +
+                "000000000000000000000").HexToByteArray();
+
+            CollectionAssert.AreEqual(expectedAssetData, assetData);
+        }
+
+    }
+}
