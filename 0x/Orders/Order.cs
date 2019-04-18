779ef5ebc35ec7759e2028a1edd9b0def43340a2
Bojan Malinic <bojan.malinic@outlook.com>
2019-04-19 00:53:50 +0200

    Add order related classes

---
+++ 0x/Orders/Order.cs
@@ -0,0 +1,53 @@
+﻿using EIP712;
+using System.Numerics;
+using ZeroX.Assets;
+using ZeroX.Utilities;
+
+namespace ZeroX.Orders
+{
+    public class Order
+    {
+        public EthereumAddress MakerAddress { get; set; }
+        public EthereumAddress TakerAddress { get; set; }
+        public EthereumAddress FeeRecipientAddress { get; set; }
+        public EthereumAddress SenderAddress { get; set; }
+        public BigInteger MakerAssetAmount { get; set; }
+        public BigInteger TakerAssetAmount { get; set; }
+        public BigInteger MakerFee { get; set; }
+        public BigInteger TakerFee { get; set; }
+        public BigInteger ExpirationTimeSeconds { get; set; }
+        public BigInteger Salt { get; set; }
+        public Asset MakerAssetData { get; set; }
+        public Asset TakerAssetData { get; set; }
+
+        public byte[] Hash(EthereumAddress exchangeAddress)
+        {
+            EIP712Domain domain = new EIP712Domain()
+            {
+                Name = "0x Protocol",
+                Version = "2",
+                VerifyingContract = exchangeAddress.ToString()
+            };
+
+            return EIP712.EIP712.Hash(EIP712Order, domain);
+        }
+
+        private OrderInternal EIP712Order
+        {
+            get => new OrderInternal
+            {
+                MakerAddress = MakerAddress.ToString(),
+                TakerAddress = TakerAddress.ToString(),
+                FeeRecipientAddress = FeeRecipientAddress.ToString(),
+                SenderAddress = SenderAddress.ToString(),
+                MakerAssetAmount = MakerAssetAmount,
+                TakerAssetAmount = TakerAssetAmount,
+                MakerFee = TakerFee,
+                ExpirationTimeSeconds = ExpirationTimeSeconds,
+                Salt = Salt,
+                MakerAssetData = MakerAssetData.AssetData,
+                TakerAssetData = TakerAssetData.AssetData
+            }; 
+        }
+    }
+}
---
+++ 0x/Orders/OrderInternal.cs
@@ -0,0 +1,45 @@
+﻿using EIP712.Attributes;
+using System.Numerics;
+using ZeroX.Utilities;
+
+namespace ZeroX.Orders
+{
+    internal class OrderInternal
+    {
+        [Member("address", 1)]
+        public string MakerAddress { get; set; }
+
+        [Member("address", 2)]
+        public string TakerAddress { get; set; }
+
+        [Member("address", 3)]
+        public string FeeRecipientAddress { get; set; }
+
+        [Member("address", 4)]
+        public string SenderAddress { get; set; }
+
+        [Member("uint256", 5)]
+        public BigInteger MakerAssetAmount { get; set; }
+
+        [Member("uint256", 6)]
+        public BigInteger TakerAssetAmount { get; set; }
+
+        [Member("uint256", 7)]
+        public BigInteger MakerFee { get; set; }
+
+        [Member("uint256", 8)]
+        public BigInteger TakerFee { get; set; }
+
+        [Member("uint256", 9)]
+        public BigInteger ExpirationTimeSeconds { get; set; }
+
+        [Member("uint256", 10)]
+        public BigInteger Salt { get; set; }
+
+        [Member("bytes", 11)]
+        public byte[] MakerAssetData { get; set; }
+
+        [Member("bytes", 12)]
+        public byte[] TakerAssetData { get; set; }
+    }
+}
