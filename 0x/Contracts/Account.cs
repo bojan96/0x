70b06d3e3f825649536a49fa1f6f1cf8e21f2a83
Bojan Malinic <bojan.malinic@outlook.com>
2019-04-23 01:20:41 +0200

    Add helper classes

---
+++ 0x/Contracts/Account.cs
@@ -0,0 +1,16 @@
+﻿using ZeroX.Utilities;
+
+namespace ZeroX.Contracts
+{
+    public class Account
+    {
+        public Account(EthereumAddress address, string privateKey)
+        {
+            Address = address;
+            PrivateKey = privateKey;
+        }
+
+        public string PrivateKey { get; set; }
+        public EthereumAddress Address { get; set; }
+    }
+}
---
+++ 0x/Contracts/TxParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace ZeroX.Contracts
+{
+    public class TxParameters
+    {
+        public BigInteger GasPrice { get; set; }
+        public int GasLimit { get; set; }
+    }
+}
