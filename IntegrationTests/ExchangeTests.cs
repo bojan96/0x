fdc7e43d90bb593701f8d39ce16fdf474e3293d7
Bojan Malinic <bojan.malinic@outlook.com>
2019-04-23 12:38:21 +0200

    Add integration tests

---
+++ IntegrationTests/ExchangeTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZeroX.Contracts;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class ExchangeTests
+    {
+
+        private const string ExchangeAddress = "0x48bacb9266a570d521063ef5dd96e61686dbe788";
+
+        [TestMethod]
+        public void TestExchange()
+        {
+            Exchange exchange = new Exchange("http://localhost:8545", Exchange)
+        }
+    }
+}
