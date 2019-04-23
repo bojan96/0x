8c6d5dd46383ad7df1b35218798bd32d33246b65
Bojan Malinic <bojan.malinic@outlook.com>
2019-04-23 08:57:55 +0200

    Add nonce

--- 0x/Utilities/TxParameters.cs
+++ 0x/Utilities/TxParameters.cs
@@ -5,12 +5,14 @@ namespace ZeroX.Utilities
     // TODO: Add argument validation
     public class TxParameters
     {
-        public TxParameters(BigInteger gasPrice, int gasLimit)
+        public TxParameters(BigInteger gasPrice, int gasLimit, BigInteger nonce)
         {
             GasPrice = gasPrice;
             GasLimit = gasLimit;
+            Nonce = nonce;
         }
         public BigInteger GasPrice { get; set; }
         public int GasLimit { get; set; }
+        public BigInteger Nonce { get; set; }
     }
 }
