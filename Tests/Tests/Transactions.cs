3dfe8d1b51f6973bd2c3c1207c2e1ab2b0e6811a
Bojan Malinic <bojan.malinic@outlook.com>
2019-05-08 13:33:20 +0200

    Add tests for Transaction class

---
+++ Tests/Tests/Transactions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nethereum.Hex.HexConvertors.Extensions;
+using ZeroX.Transactions;
+using ZeroX.Utilities;
+
+namespace Tests.Tests
+{
+    [TestClass]
+    public class Transactions
+    {
+        [TestMethod]
+        public void HashTransaction()
+        {
+            Transaction tx = new Transaction(EthereumAddress.ZeroAddress, "0x00");
+            byte[] hash = tx.Hash(EthereumAddress.ZeroAddress);
+            byte[] expectedHash = "0x260d10cec3971fd8a2037ea41e9b4f2658fb1bbf15984ae8428c2f8f6e3408ee".HexToByteArray();
+
+            CollectionAssert.AreEqual(expectedHash, hash);
+        }
+    }
+}
