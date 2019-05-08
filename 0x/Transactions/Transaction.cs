7fabb74e5042d45fd2c1c2d37f991aaf606fbdd9
Bojan Malinic <bojan.malinic@outlook.com>
2019-05-08 13:33:07 +0200

    Add Transaction class

---
+++ 0x/Transactions/Transaction.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using ZeroX.Utilities;
+using Nethereum.Hex.HexConvertors.Extensions;
+
+[assembly: InternalsVisibleTo("Tests")]
+
+
+namespace ZeroX.Transactions
+{
+    public class Transaction
+    {
+
+        public BigInteger Salt { get; }
+        public EthereumAddress SignerAddress { get; }
+        public string TxData { get; }
+
+        internal Transaction(EthereumAddress signerAddress, string txData)
+        {
+            Salt = 0; // Make random
+            SignerAddress = signerAddress;
+            TxData = txData;
+        }
+
+        internal TransactionEIP712 EIP712Transaction
+        {
+            get => new TransactionEIP712
+            {
+                Salt = Salt,
+                SignerAddress = SignerAddress,
+                Data = TxData.HexToByteArray()
+            };
+        }
+
+        public byte[] Hash(EthereumAddress exchangeAddress)
+        {
+            var hash = EIP712.EIP712.Hash(EIP712Transaction, new EIP712.EIP712Domain
+            {
+                Name = "0x Protocol",
+                Version = "2",
+                VerifyingContract = exchangeAddress
+            });
+
+            return hash;
+        }
+
+    }
+}
---
+++ 0x/Transactions/TransactionEIP712.cs
@@ -0,0 +1,19 @@
+﻿using EIP712.Attributes;
+using System;
+using System.Numerics;
+
+namespace ZeroX.Transactions
+{
+    [StructName("ZeroExTransaction")]
+    internal class TransactionEIP712
+    {
+        [Member("uint256", 1)]
+        public BigInteger Salt { get; set; }
+
+        [Member("address", 2)]
+        public string SignerAddress { get; set; }
+
+        [Member("bytes", 3)]
+        public byte[] Data { get; set; }
+    }
+}
