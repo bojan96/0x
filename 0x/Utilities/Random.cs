65f5eb6d5a5696b98430ab3af422f91904c01f1f
Bojan Malinic <bojan.malinic@outlook.com>
2019-05-10 14:58:18 +0200

    Add Random class

---
+++ 0x/Utilities/Random.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace ZeroX.Utilities
+{
+    internal static class Random
+    {
+
+        private static System.Random _random = new System.Random();
+
+        /// <summary>
+        /// Generates a salt value suitable for use in orders and 0x transactions
+        /// </summary>
+        /// <returns></returns>
+        public static BigInteger GenerateSalt()
+        {
+            byte[] randomInt = new byte[33];
+            randomInt[32] = 0; // Ensure we will not generate negative value
+            _random.NextBytes(randomInt);
+
+            return new BigInteger(randomInt);
+        }
+    }
+}
