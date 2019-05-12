861a2ca8838439638126ed8eef146d8a227d05a8
Bojan Malinic <bojan.malinic@outlook.com>
2019-05-13 01:21:25 +0200

    Make GenerateSalt thread safe

--- 0x/Utilities/Random.cs
+++ 0x/Utilities/Random.cs
@@ -6,6 +6,7 @@ namespace ZeroX.Utilities
     {
 
         private static System.Random _random = new System.Random();
+        private static object _randLock = new object();
 
         /// <summary>
         /// Generates a salt value suitable for use in orders and 0x transactions
@@ -14,7 +15,8 @@ namespace ZeroX.Utilities
         public static BigInteger GenerateSalt()
         {
             byte[] randomInt = new byte[33];
-            _random.NextBytes(randomInt);
+            lock(_randLock)
+                _random.NextBytes(randomInt);
             randomInt[32] = 0; // Ensure we will not generate negative value
 
             return new BigInteger(randomInt);
