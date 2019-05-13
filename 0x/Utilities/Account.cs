2db0b03c7c00e17c64389f840c755c37d0558c79
Bojan Malinic <bojan.malinic@outlook.com>
2019-05-13 10:00:43 +0200

    Add ctor argument validation

--- 0x/Utilities/Account.cs
+++ 0x/Utilities/Account.cs
@@ -1,17 +1,39 @@
 ﻿using Nethereum.Web3;
+using Nethereum.Hex.HexConvertors.Extensions;
+using System;
 
 namespace ZeroX.Utilities
 {
-    // TODO: Add argument validation
     public class Account
     {
+        /// <summary>
+        /// Creates ethereum account from private key
+        /// </summary>
+        /// <param name="privateKey"></param>
         public Account(string privateKey)
         {
+            if (privateKey == null)
+                throw new ArgumentNullException(nameof(privateKey));
+            if (!ValidatePrivateKey(privateKey))
+                throw new ArgumentException("Invalid private key", nameof(privateKey));
             PrivateKey = privateKey;
             Address = (EthereumAddress)Web3.GetAddressFromPrivateKey(privateKey);
         }
 
-        public string PrivateKey { get; set; }
-        public EthereumAddress Address { get; set; }
+        public string PrivateKey { get; }
+        public EthereumAddress Address { get; }
+
+        private bool ValidatePrivateKey(string privateKey)
+        {
+            try
+            {
+                byte[] privateKeyByte = privateKey.HexToByteArray();
+                return privateKeyByte.Length == 32;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
     }
 }
