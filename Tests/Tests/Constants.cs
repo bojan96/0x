68321af9b39e8727c644ddc0d0ed5b6872f75e71
Bojan Malinic <bojan.malinic@outlook.com>
2019-05-27 18:22:13 +0200

    Add private key

--- Tests/Tests/Constants.cs
+++ Tests/Tests/Constants.cs
@@ -2,6 +2,9 @@
 {
     public static class Constants
     {
-        public const string TestEthereumAddress = "0x646934251e9045F41DdF7145Dea5cD63cD45d8Da";
+        public const string TestEthereumAddress 
+            = "0x5409ED021D9299bf6814279A6A1411A7e866A631";
+        public const string TestPrivateKey
+            = "0xf2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d";
     }
 }
