f15acf09c9c9ff6082b5f6129eb870b4dff24bba
Bojan Malinic <bojan.malinic@outlook.com>
2019-05-29 11:34:54 +0200

    Add internal specifier

--- 0x/Utilities/DateExtensions.cs
+++ 0x/Utilities/DateExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace ZeroX.Utilities
 {
-    public static class DateExtensions
+    internal static class DateExtensions
     {
         private static readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
