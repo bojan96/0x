a02ff2b452777dc6f7c35726fd1d6d53bf59d4f6
Bojan Malinić <bojan.malinic@outlook.com>
2019-05-31 16:49:52 +0200

    Add DateExtension methods (#7)
    * Add DateExtension methods
    
    * Fix typo

--- 0x/Utilities/DateExtensions.cs
+++ 0x/Utilities/DateExtensions.cs
@@ -2,12 +2,15 @@
 
 namespace ZeroX.Utilities
 {
-    internal static class DateExtensions
+    public static class DateExtensions
     {
         private static readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         public static long GetUnixTime(this DateTime dateTime)
             => (long)(dateTime.ToUniversalTime() - _epoch).TotalSeconds;
+
+        public static DateTime GetUtcDate(this long unixTime)
+            => _epoch + TimeSpan.FromSeconds(unixTime);
         
     }
 }
---
+++ Tests/Tests/DateTimeTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ZeroX.Utilities;
+
+namespace Tests.Tests
+{
+    [TestClass]
+    public class DateTimeTests
+    {
+        [TestMethod]
+        public void DateTimeUnixConversion()
+        {
+            DateTime expectedDt = DateTime.UtcNow;
+            var dt = expectedDt.GetUnixTime().GetUtcDate();
+            Assert.AreEqual(expectedDt.Date, dt.Date);
+            Assert.AreEqual(expectedDt.Second, dt.Second);
+            Assert.AreEqual(expectedDt.Minute, dt.Minute);
+            Assert.AreEqual(expectedDt.Hour, dt.Hour);
+
+            // Millisecond part is going to differ obviously
+        }
+    }
+}
