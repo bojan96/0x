adbefd1cfda38782d3da95e0d450758684a4aae9
Bojan Malinic <bojan.malinic@outlook.com>
2019-06-04 14:15:33 +0200

    Remove asset factory, add asset utilities

---
+++ 0x/Assets/AssetUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ZeroX.Assets
+{
+    public static class AssetUtil
+    {
+
+        /// <summary>
+        /// Constructs one of the supported asset types, also cast to given asset type 
+        /// </summary>
+        /// <typeparam name="T">Asset type</typeparam>
+        /// <param name="assetData">Asset data</param>
+        /// <returns>New instance</returns>
+        /// <exception cref="InvalidCastException">Unable to cast to specified asset data</exception>
+        public static T Create<T>(byte[] assetData) where T : Asset
+        {
+            Asset asset;
+
+            if (ERC20Asset.ValidateAssetData(assetData))
+                asset = ERC20Asset.Create(assetData);
+            else if (ERC721Asset.ValidateAssetData(assetData))
+                asset = ERC721Asset.Create(assetData);
+            else
+                throw new ArgumentException("Not valid asset data", nameof(assetData));
+
+           return (T)asset;
+        }
+    }
+}
